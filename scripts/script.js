33d73ae3fdae5f1a791bc52df643efb845df05ea
Naveen2905 <naveenmalhotra05@gmail.com>
2020-05-13 17:59:31 -0400

    getting the values from the form

--- scripts/script.js
+++ scripts/script.js
@@ -12,15 +12,36 @@ function formSlideIn(e) {
 
 //get the data
 
-function getData() {
-    const getFormData = document.querySelector('.jobDetailForm input').value;
+// function getData() {
+//     const getFormData = document.querySelector('.jobDetailForm input').value;
 
-    console.log(getFormData);
+//     console.log(getFormData);
     
 
-    const dataOutput = Array.from(document.querySelectorAll('.jobDetailForm input')).reduce((acc, input) => ({ ...acc, [input.placeholder]: input.value }), {});
+//     const dataOutput = Array.from(document.querySelectorAll('.jobDetailForm input')).reduce((acc, input) => ({ ...acc, [input.placeholder]: input.value }), {});
 
-    console.log(dataOutput);
+//     console.log(dataOutput);
     
     
-}
+// }
+
+const formElement = document.querySelector('.jobDetailForm');
+
+
+formElement.addEventListener('submit', function(e) {
+    e.preventDefault();
+    //after submit it closes the form
+    formSection.style.left = '-100%';
+
+    const companyName = document.querySelector('.cName').value;
+    const jobRole = document.querySelector('.jobTitle').value;
+    const dateApplied = document.querySelector('.date').value;
+    const jobSource = document.querySelector('.source').value;
+    const fullName = document.querySelector('.fullName').value;
+    const emailAdd = document.querySelector('.email').value;
+    const urlLinkedin = document.querySelector('.linkedin').value;
+    const jobTitle = document.querySelector('.title').value;
+
+    
+    
+})
